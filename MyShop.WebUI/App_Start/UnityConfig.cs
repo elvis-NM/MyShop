2c757a3cfa970fd195bf9d3d03562ca04d6041dc
elvis_makia <elvis_makia@yahoo.com>
2020-11-22 18:52:45 +0000

    reviewed model fro errors

--- MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop.WebUI/App_Start/UnityConfig.cs
@@ -3,6 +3,8 @@ using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
 using MyShop.DataAccess.SQL;
 using System;
+using MyShop.Services;
+
 
 using Unity;
 
@@ -48,6 +50,10 @@ namespace MyShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
+
         }
     }
 }
