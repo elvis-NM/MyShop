e6cedeac94681ebbcdbf440096223e7d2c867ed0
elvis_makia <elvis_makia@yahoo.com>
2020-11-23 04:28:21 +0000

    Revert "adjusted folder"
    This reverts commit 235f547ff119e8984ac6dbd242ffe4aa968621a4.

---
+++ MyShop.WebUI.Tests/Controllers/BasketControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.WebUI.Tests.Mocks;
+using System;
+using MyShop.Services;
+using MyShop.WebUI.Tests.Controllers.Mocks;
+using System.Linq;
+
+namespace MyShop.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class BasketControllerTests
+    {
+        [TestMethod]
+        public void CanAddBasketItem()
+        {
+            IRepository<Basket> baskets = new MockContext<Basket>();
+            IRepository<Product> products = new MockContext<Product>();
+
+
+            var httpContext = new MockHttpContext();
+
+            IBasketService basketService = new BasketService (products, baskets);
+
+            basketService.AddToBasket(httpContext, "1");
+
+            Basket basket = baskets.Collection().FirstOrDefault();
+
+            Assert.IsNotNull(basket);
+            Assert.AreEqual(1, basket.BasketItems.Count);
+            Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+    }
+}
+
